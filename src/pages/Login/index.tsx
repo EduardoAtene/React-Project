e78f8d7a9d873d9d573e57d74a6f86889b01e409
EduardoAtene <eduardoatenesilvae@gmail.com>
2023-07-03 18:52:01 -0300

    subindo CORS

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -123,7 +123,22 @@ export function Login() {
     registerSchema
       .validate(values, { abortEarly: false })
       .then(() => {
-          axios.post(API_URL + 'usuario')
+          fetch(API_URL + 'usuario', {
+            method: "POST", // *GET, POST, PUT, DELETE, etc.
+            mode: "no-cors", // no-cors, *cors, same-origin
+            cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+            credentials: "same-origin", // include, *same-origin, omit
+            headers: {
+              "Content-Type": "application/json",
+              // 'Content-Type': 'application/x-www-form-urlencoded',
+            },
+            redirect: "follow", // manual, *follow, error
+            referrerPolicy: "no-referrer", // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+            body: JSON.stringify(values)
+          }).then((response) => {
+            console.log(response);
+          });
+      
 
           // .register(values)
           // .then(response => {
